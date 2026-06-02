2c2f0126d2c7fcae5c9b0230671dabdb8c299294
zaytsev.p.v <Zaytsev.P.V@sbertech.ru>
2026-06-02 23:28:37 +0400

    заменил локаторы на поля через @FindBy

--- src/main/java/ru/otus/pages/RegistrationFormPage.java
+++ src/main/java/ru/otus/pages/RegistrationFormPage.java
@@ -3,11 +3,11 @@ package ru.otus.pages;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,14 +15,29 @@ import ru.otus.model.RegistrationData;
 
 public class RegistrationFormPage {
 
-    private static final By USERNAME_INPUT = By.id("username");
-    private static final By EMAIL_INPUT = By.id("email");
-    private static final By PASSWORD_INPUT = By.id("password");
-    private static final By CONFIRM_PASSWORD_INPUT = By.id("confirm_password");
-    private static final By BIRTHDATE_INPUT = By.id("birthdate");
-    private static final By LANGUAGE_LEVEL_SELECT = By.id("language_level");
-    private static final By SUBMIT_BUTTON = By.cssSelector("input[type='submit'], button[type='submit']");
-    private static final By OUTPUT_BLOCK = By.id("output");
+    @FindBy(id = "username")
+    private WebElement usernameInput;
+
+    @FindBy(id = "email")
+    private WebElement emailInput;
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+    @FindBy(id = "confirm_password")
+    private WebElement confirmPasswordInput;
+
+    @FindBy(id = "birthdate")
+    private WebElement birthdateInput;
+
+    @FindBy(id = "language_level")
+    private WebElement languageLevelSelect;
+
+    @FindBy(css = "input[type='submit'], button[type='submit']")
+    private WebElement submitButton;
+
+    @FindBy(id = "output")
+    private WebElement outputBlock;
 
     private final WebDriver driver;
     private final WebDriverWait wait;
@@ -31,6 +46,7 @@ public class RegistrationFormPage {
     public RegistrationFormPage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
     }
 
     public RegistrationFormPage open(String url) {
@@ -39,17 +55,17 @@ public class RegistrationFormPage {
     }
 
     public RegistrationFormPage fillForm(RegistrationData data) {
-        type(USERNAME_INPUT, data.username());
-        type(EMAIL_INPUT, data.email());
-        type(PASSWORD_INPUT, data.password());
-        type(CONFIRM_PASSWORD_INPUT, data.confirmPassword());
+        type(usernameInput, data.username());
+        type(emailInput, data.email());
+        type(passwordInput, data.password());
+        type(confirmPasswordInput, data.confirmPassword());
         setDate(data.birthdate());
         selectLanguageLevel(data.languageLevel());
         return this;
     }
 
     public RegistrationFormPage submit() {
-        visible(SUBMIT_BUTTON).click();
+        visible(submitButton).click();
         return this;
     }
 
@@ -59,19 +75,19 @@ public class RegistrationFormPage {
 
     public String resultText() {
         return wait.until(driver -> {
-            String text = driver.findElement(OUTPUT_BLOCK).getText();
+            String text = outputBlock.getText();
             return text == null || text.isBlank() ? null : text;
         });
     }
 
-    private void type(By locator, String text) {
-        WebElement element = visible(locator);
-        element.clear();
-        element.sendKeys(text);
+    private void type(WebElement element, String text) {
+        WebElement visibleElement = visible(element);
+        visibleElement.clear();
+        visibleElement.sendKeys(text);
     }
 
     private void setDate(String isoDate) {
-        WebElement element = visible(BIRTHDATE_INPUT);
+        WebElement element = visible(birthdateInput);
 
         ((JavascriptExecutor) driver).executeScript(
                 "arguments[0].value = arguments[1];" +
@@ -87,17 +103,18 @@ public class RegistrationFormPage {
     }
 
     private void selectLanguageLevel(String languageLevel) {
-        Select select = new Select(visible(LANGUAGE_LEVEL_SELECT));
+        Select select = new Select(visible(languageLevelSelect));
         select.selectByVisibleText(languageLevel);
 
         WebElement selectedOption = select.getFirstSelectedOption();
+
         languageLevelResultVariants.clear();
         languageLevelResultVariants.add(languageLevel);
         languageLevelResultVariants.add(selectedOption.getText());
         languageLevelResultVariants.add(selectedOption.getAttribute("value"));
     }
 
-    private WebElement visible(By locator) {
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    private WebElement visible(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element));
     }
 }
